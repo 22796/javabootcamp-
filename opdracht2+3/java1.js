a14c247bee0189fdf118dd140f682c59d4652382
22796 <22796@ma-web.nl>
2017-09-15 10:24:32 +0200

    opdracht 2+3 geupload

---
+++ opdracht2+3/java1.js
@@ -0,0 +1,22 @@
+var firstName1 = "Frank";
+var lastName1 = "Muhren";
+var age1 = 18;
+var isBoy1 = true;
+var isTired1 = true;
+var hobby1 = "gaming";
+
+
+var firstName2 = "Nick";
+var lastName2 = "Hordijk";
+var age2 = 22;
+var isBoy2 = true;
+var isTired2 = false;
+var hobby2 = "golf";
+
+
+var firstName3 = "Melissa";
+var lastName3 = "Vriend";
+var age3 = 18;
+var isBoy3 = false;
+var isTired3 = true;
+var hobby3 = "gaming";
