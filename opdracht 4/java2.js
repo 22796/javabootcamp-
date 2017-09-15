20844cb4fc792aeb7f5d72f697e8148a19602f34
22796 <22796@ma-web.nl>
2017-09-15 10:24:50 +0200

    uploaded opdracht 4

---
+++ opdracht 4/java2.js
@@ -0,0 +1,25 @@
+//1=frank 2=nick 3=melissa
+
+//persoon 1: frank Muhren.
+var firstName1 = "Frank";
+var lastName1 = "Muhren";
+var age1 = 18;
+var isBoy1 = true;
+var isTired1 = true;
+var hobby1 = "gaming";
+
+//persoon 2: Nick Hordijk.
+var firstName2 = "Nick";
+var lastName2 = "Hordijk";
+var age2 = 22;
+var isBoy2 = true;
+var isTired2 = false;
+var hobby2 = "golf";
+
+//persoon 3: Melissa vriend.
+var firstName3 = "Melissa";
+var lastName3 = "Vriend";
+var age3 = 18;
+var isBoy3 = false;
+var isTired3 = true;
+var hobby3 = "gaming";
