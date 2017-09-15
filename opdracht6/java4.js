74e79636bc50d302f95262192db422a6c20111fc
22796 <22796@ma-web.nl>
2017-09-15 12:12:35 +0200

    uploaded opdracht 6

---
+++ opdracht6/java4.js
@@ -0,0 +1,46 @@
+//1=Melissa 2=Nickoline 3=Frank
+
+//persoon 1: melissa Vriend.
+var firstName1 = "Melissa";
+var lastName1 = "Vriend";
+var age1 = 11;
+var isBoy1 = false;
+var isTired1 = false;
+var hobby1 = "golf";
+
+console.log(typeof firstName1);
+console.log(typeof lastName1);
+console.log(typeof age1);
+console.log(typeof isBoy1);
+console.log(typeof isTired1);
+console.log(typeof hobby1);
+
+//persoon 2: Nick Hordijk.
+var firstName2 = "Nickoline";
+var lastName2 = "Hordijk";
+var age2 = 22;
+var isBoy2 = false;
+var isTired2 = true;
+var hobby2 = "gaming";
+
+console.log(typeof firstName2);
+console.log(typeof lastName2);
+console.log(typeof age2);
+console.log(typeof isBoy2);
+console.log(typeof isTired2);
+console.log(typeof hobby2);
+
+//persoon 3: Frank Mühren.
+var firstName3 = "Frank";
+var lastName3 = "Muhren";
+var age3 = 20;
+var isBoy3 = true;
+var isTired3 = false;
+var hobby3 = "golf";
+
+console.log(typeof firstName3);
+console.log(typeof lastName3);
+console.log(typeof age3);
+console.log(typeof isBoy3);
+console.log(typeof isTired3);
+console.log(typeof hobby3);
