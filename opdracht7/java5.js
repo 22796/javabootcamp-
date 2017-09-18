0bb79728ab82adab631fba0fe8a8ea4305fddb36
22796 <22796@ma-web.nl>
2017-09-18 09:48:18 +0200

    Add files via upload

---
+++ opdracht7/java5.js
@@ -0,0 +1,60 @@
+//1=Melissa 2=Nickoline 3=Frank
+
+//persoon 1: melissa Vriend.
+var firstName1 = "Melissa";
+var lastName1 = "Vriend";
+var age1 = 11;
+var isBoy1 = false;
+var isTired1 = false;
+var hobby1 = "golf";
+
+console.log(typeof firstName1);
+console.log(typeof lastName1);
+console.log(typeof age1);
+console.log(typeof isBoy1);
+console.log(typeof isTired1);
+console.log(typeof hobby1);
+
+//persoon 2: Nick Hordijk.
+var firstName2 = "Nickoline";
+var lastName2 = "Hordijk";
+var age2 = 22;
+var isBoy2 = false;
+var isTired2 = true;
+var hobby2 = "gaming";
+
+console.log(typeof firstName2);
+console.log(typeof lastName2);
+console.log(typeof age2);
+console.log(typeof isBoy2);
+console.log(typeof isTired2);
+console.log(typeof hobby2);
+
+//persoon 3: Frank Mühren.
+var firstName3 = "Frank";
+var lastName3 = "Muhren";
+var age3 = 20;
+var isBoy3 = true;
+var isTired3 = false;
+var hobby3 = "golf";
+
+console.log(typeof firstName3);
+console.log(typeof lastName3);
+console.log(typeof age3);
+console.log(typeof isBoy3);
+console.log(typeof isTired3);
+console.log(typeof hobby3);
+
+//vars optellen
+console.log(typeof(firstName1)+"+"+typeof(firstName2)+"="+typeof(firstName1 + firstName2)); //string plus string
+console.log(typeof(firstName1)+"+"+typeof(isBoy1)+"="+typeof(firstName1 + isBoy1)); //string plus boolean
+console.log(typeof(age3)+"+"+typeof(isBoy2)+"="+typeof(age3 + isBoy2)); // nummer plus boolean
+console.log(3+"3");
+
+
+
+
+// var result=document.getElementById("result1");
+// result.innerHTML="Hallo ik ben "+(firstName3)+" en ik ben " +age3;
+// var result=document.getElementById("result2");
+// result.innerHTML="Hallo ik ben "+typeof(firstName3)+" en ik ben " +typeof(age3);
